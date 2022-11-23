548325784d1f3b15b727b0d2fbc50ef477a7fbd5
vgogichashvili47@gmail.com <98263919+Vgogichashvili@users.noreply.github.com>
2022-11-23 11:53:52 +0400

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
    firebaseConfig : {
-  apiKey: "AIzaSyDb3BtYY5ap-3YR3BiaVqwmwt_SkNbDt0c",
-  authDomain: "staybnb3.firebaseapp.com",
-  projectId: "staybnb3",
-  storageBucket: "staybnb3.appspot.com",
-  messagingSenderId: "155331336782",
-  appId: "1:155331336782:web:d1a7c7dc168744587f011f",
-  measurementId: "G-SBHS037J88"
+  apiKey: "AIzaSyDsdGLGXOiY0-48g-fwi64di3sfSeMKq_0",
+  authDomain: "staybnb4.firebaseapp.com",
+  projectId: "staybnb4",
+  storageBucket: "staybnb4.appspot.com",
+  messagingSenderId: "242761732208",
+  appId: "1:242761732208:web:39c47259f3665df8bc24b5",
+  measurementId: "G-D23PP6DX1S"
   }
 };
 
